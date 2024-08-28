aaca742d7bfec1bfd156b1365ebce38a661a745f
Gustavo <gustavosmsg18@gmail.com>
2024-08-28 20:41:57 -0300

    Condicionais Java

---
+++ controle-fluxo/src/PlanoOperadora.java
@@ -0,0 +1,21 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        String plano = "B"; // B/ M /T
+
+        switch (plano) { //SWITCH CASE
+            case "T": {
+                System.out.println("5GB Youtube");
+                break;
+            }
+            case "M": {
+                System.out.println("Whats e Instagram gratis");
+                break;
+            }
+            case "B": {
+                System.out.println("100 minutos de ligação");
+                break;
+            }
+          
+        }
+    }
+}
